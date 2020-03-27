3963dab110bb5b2dd3695ec68adc53d14e881c17
MinexAutomation <minex.automation@gmail.com>
2020-03-26 19:23:45 -0700

    Added IWebStartup.

---
+++ source/R5T.Dover.Abstractions/Code/Interfaces/IWebStartup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Builder;
+
+using R5T.Richmond;
+
+
+namespace R5T.Dover
+{
+    public interface IWebStartup : IStartup
+    {
+        void Configure(IApplicationBuilder applicationBuilder);
+    }
+}
